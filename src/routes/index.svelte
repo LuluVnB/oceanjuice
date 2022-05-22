5778df62d525b16383cb0954e671444b01e708ef
Senofy <hello@digitalera.dev>
2022-05-22 10:52:04 -0700

    Added information squares

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -20,6 +20,11 @@
         <h1>Our mission is: <span class='typing'></span></h1>
     </TypedJs>
 </header>
+<section class="information">
+    <div class="airgun"><h2>Airgun</h2><h3>225 dB</h3><p>An airgun in the water is comparable to a human standing next to a rocket engine when it's launching.</p></div>
+    <div class="sonar"><h2>Propeller</h2><h3>180 dB</h3><p>It is comparable to standing next to a 12 gauge shotgun blast</p></div>
+    <div class="animal"><h2>Sea Creature Info</h2><p>Noise is amplified in the ocean because sound travels faster in the water. Because of this, sea creatures are fatally affected by noise. Noises as simple as a propeller from a ship to a whale is like firing a 12 gauged shot gun next to a human's ear. Noise pollution disrupts creatures lives as they rely on sonar and echolocation to communicate. Various noises caused by humans have caused many of these species, to get lost, leading to ocean activity decrease every day. Some creatures, such as whales, are detrimentally affected because hearing loss can lead to massive amounts of damage and even death.</p></div>
+</section>
 
 <style>
     nav {
@@ -69,5 +74,50 @@
     .typing {
         font-size: 48px;
         color: white;
+    }
+
+    .information {
+        width: 100%;
+
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        grid-template-rows: auto auto;
+        grid-template-areas: 'airgun sonar' 'animal animal';
+        justify-items: center;
+    }
+
+    .information > div {
+        width: 100%;
+        text-align: center;
+        padding: 64px 32px;
+        box-sizing: border-box;
+    }
+
+    .information h2 {
+        font-size: 36px;
+    }
+
+    .information h3 {
+        margin-top: 16px;
+        font-size: 24px;
+    }
+
+    .information p {
+        margin-top: 8px;
+    }
+
+    .airgun {
+        grid-area: airgun;
+        background-color: pink;
+    }
+
+    .sonar {
+        grid-area: sonar;
+        background-color: green;
+    }
+
+    .animal {
+        grid-area: animal;
+        background-color: blue;
     }
 </style>
