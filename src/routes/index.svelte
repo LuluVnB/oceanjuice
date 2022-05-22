898197d4989b39d7692866df3a489c6a4d7610d4
Senofy <hello@digitalera.dev>
2022-05-22 11:14:06 -0700

    Added placeholder images and made it loop again

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,7 +4,7 @@
 import LoadingScreen from '../lib/LoadingScreen.svelte';
 </script>
 
-<LoadingScreen />
+<!-- <LoadingScreen /> -->
 <nav>
     <img src="/tritonhacks-logo.webp" alt="">
     <div>
@@ -14,7 +14,7 @@ import LoadingScreen from '../lib/LoadingScreen.svelte';
 </nav>
 <header>
     <video autoplay loop>
-        <source src="/video.mp4" autoplay type="video/mp4">
+        <source src="/video.mp4" autoplay loop type="video/mp4">
         Your browser does not support the video tag
     </video>
     <TypedJs strings={['Saving Ocean Animals', 'Drinking Ocean Water', "Attacking Ocean Wildlife", 
@@ -24,7 +24,7 @@ import LoadingScreen from '../lib/LoadingScreen.svelte';
 </header>
 <section class="information">
     <div class="airgun"><h2>Airgun</h2><h3>225 dB</h3><p>An airgun in the water is comparable to a human standing next to a rocket engine when it's launching.</p></div>
-    <div class="sonar"><h2>Propeller</h2><h3>180 dB</h3><p>It is comparable to standing next to a 12 gauge shotgun blast</p></div>
+    <div class="propeller"><h2>Propeller</h2><h3>180 dB</h3><p>It is comparable to standing next to a 12 gauge shotgun blast</p></div>
     <div class="animal"><h2>Sea Creature Info</h2><p>Noise is amplified in the ocean because sound travels faster in the water. Because of this, sea creatures are fatally affected by noise. Noises as simple as a propeller from a ship to a whale is like firing a 12 gauged shot gun next to a human's ear. Noise pollution disrupts creatures lives as they rely on sonar and echolocation to communicate. Various noises caused by humans have caused many of these species, to get lost, leading to ocean activity decrease every day. Some creatures, such as whales, are detrimentally affected because hearing loss can lead to massive amounts of damage and even death.</p></div>
 </section>
 
@@ -84,7 +84,7 @@ import LoadingScreen from '../lib/LoadingScreen.svelte';
         display: grid;
         grid-template-columns: 1fr 1fr;
         grid-template-rows: auto auto;
-        grid-template-areas: 'airgun sonar' 'animal animal';
+        grid-template-areas: 'airgun propeller' 'animal animal';
         justify-items: center;
     }
 
@@ -93,6 +93,11 @@ import LoadingScreen from '../lib/LoadingScreen.svelte';
         text-align: center;
         padding: 64px 32px;
         box-sizing: border-box;
+        background-repeat: no-repeat;
+        background-position: center;
+        background-size: cover;
+        
+        color: white;
     }
 
     .information h2 {
@@ -111,15 +116,17 @@ import LoadingScreen from '../lib/LoadingScreen.svelte';
     .airgun {
         grid-area: airgun;
         background-color: pink;
+        background: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('https://media.discordapp.net/attachments/973416247935004682/977994134776344576/IMG_8431.jpg');   
     }
 
-    .sonar {
-        grid-area: sonar;
+    .propeller {
+        grid-area: propeller;
         background-color: green;
+        background: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('https://media.discordapp.net/attachments/973416247935004682/977994134776344576/IMG_8431.jpg');   
     }
 
     .animal {
         grid-area: animal;
-        background-color: blue;
+        background: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('https://media.discordapp.net/attachments/973416247935004682/977994134776344576/IMG_8431.jpg');   
     }
 </style>
